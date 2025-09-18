473ae2e6de5398aa386e3e94bd5e2e614816cd7a
suelakajtazi <kajtazisuela1@gmail.com>
2025-09-18 20:37:37 +0200

    done for the dayyy

--- lesson_18/main.py
+++ lesson_18/main.py
@@ -1,29 +1,57 @@
-import pandas as pd
+
+
 import streamlit as st
+import pandas as pd
 import plotly.express as px
 
-# df = pd.DataFrame({
-
-#       'name':[]
-
-# }
-# )
 
 books_df = pd.read_csv('file.csv')
 
-st.title('bestselling books')
-st.write('this app shows the bestselling apps on amazone from 2009 to 2022')
+
+st.title("Bestselling Books Analysis")
+st.write("This app analyzes the Amazon Top Selling books from 2009 to 2022.")
 
 
-st.subheader('book statistics')
+st.subheader("Summary Statistics")
 total_books = books_df.shape[0]
-unique_title = books_df['name'].unique()
-average_rating = books_df['user rating'].mean()
-average_price = books_df['price'].mean()
+unique_titles = books_df['Name'].nunique()
+average_rating = books_df['User Rating'].mean()
+average_price = books_df['Price'].mean()
 
-col1 , col2 ,col3 ,col4 = st.columns(4)
-col1.metric('total books',total_books)
-col2.metric('titles',unique_title)
-col3.metric('average rating',average_rating)
-col4.metric('average price',average_price)
 
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total Books", total_books)
+col2.metric("Unique Titles", unique_titles)
+col3.metric("Average Rating", f"{average_rating:.2f}")
+col4.metric("Average Price", f"${average_price:.2f}")
+
+st.subheader("statistics")
+st.write(books_df.head())
+
+col1 , col2 = st.columns(2 , gap="small")
+
+with col1:
+    st.subheader("the top books")
+    top_title = books_df["Name"].value_counts().head(10)
+    st.bar_chart(top_title)
+
+with col2:
+    st.subheader("the top authors")
+    top_authors = books_df["Author"].value_counts().head(10)
+    st.bar_chart(top_authors)
+
+st.subheader("genre")
+fig = px.pie(books_df, names="Genre", title="most liked genre 2009-2022",color="Genre",
+             color_discrete_sequence=px.colors.sequential.Plasma)
+st.plotly_chart(fig)
+
+st.subheader("top 15 authors")
+top_authors = books_df['Author'].value_counts().head(15).reset_index()
+top_authors.columns=['Author','Count']
+
+figg = px.bar(top_authors, x='Count', y='Author', orientation='h',
+              title='top 15 authors',
+              labels={'Count': 'counts of books published','Author':'Author'},
+              color = 'Count',color_continuous_scale=px.colors.sequential.Plasma)
+
+st.plotly_chart(figg)
