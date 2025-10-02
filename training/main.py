0c22ce9aa632f8e95ba799f64936876ba3b127c5
suelakajtazi <kajtazisuela1@gmail.com>
2025-10-02 20:41:29 +0200

    final

--- training/main.py
+++ training/main.py
@@ -12,5 +12,16 @@ f_count = injuries[injuries['smoking_status'] == 'Former'].shape[0]
 st.title('How many people approximately get diabetes?')
 st.write(f'That’s a question that’s rarely asked since around {number:,} get it.')
 st.write(f'And most of them do at around the age {age:.1f}.')
-st.write(f'Around {never_count:,} people never smoked.')
-st.write(f'Around {f_count:,} people formaly smoked.')
+# st.write(f'Around {never_count:,} people never smoked.')
+# st.write(f'Around {f_count:,} people formaly smoked.')
+
+
+col1 ,col2 = st.columns(2)
+
+with col1:
+    st.header("people who never smoked")
+    st.write(never_count)
+
+with col2:
+    st.header("people whosmoked")
+    st.write(f_count)
