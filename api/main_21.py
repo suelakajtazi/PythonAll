270251b796b2c79206e7bd2ff35b3872e46407d5
suelakajtazi <kajtazisuela1@gmail.com>
2025-10-14 20:46:35 +0200

    Update main_21.py

--- api/main_21.py
+++ api/main_21.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/")
-def home():
-    return {'message':'Halloween'}
+# @app.get("/")
+# def home():
+#     return {'message':'Halloween'}
 
-@app.get("/user/{user_id}")
-def get_user(user_id:int):
-    return{"user_id":user_id,"name":"john doe"}
+# @app.get("/user/{user_id}")
+# def get_user(user_id:int):
+#     return{"user_id":user_id,"name":"john doe"}
 
+
+@app.get("/items/")
+def readitems():
+    return{"item":["item1","item2","item3"]}
+
+@app.post("/items/")
+def createitem(name:str,price:foat):
+    return{"item+name":name,"item_price":price}
