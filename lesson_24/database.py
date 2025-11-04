bb00ee767c2e28d0523bd7b2d8ba83efcb78271a
suelakajtazi <kajtazisuela1@gmail.com>
2025-11-04 20:40:22 +0100

    final for today

--- lesson_24/database.py
+++ lesson_24/database.py
@@ -20,3 +20,29 @@ cursor.execute('''
     INSERT INTO employees (name, position, departament, salary)
     VALUES(?,?,?,?)               
 ''',('JOHN' , 'DEVELOPER' ,'IT', '700.00'))
+
+connect.commit()
+
+cursor.execute ('SELECT * FROM employees')
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.execute('''
+   UPDATE employees
+   SET salary = ?
+   WHERE name = ?
+''', ('75000.00','JOHN'))
+
+
+connect.commit()
+
+cursor.execute('''
+    DELETE FROM employees
+    WHERE name = ?
+    ''',('JOHN',))
+
+connect.commit()
+cursor.close()
+connect.close()
